55b8816e80c6043a802fa4f141722bd26330110d
Benjamin Brahmer <info@b-brahmer.de>
2022-01-23 10:50:23 +0100

    add fcgi

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='ffshmon',
+    version='0.0.1',
     packages=['ffshmon'],
     include_package_data=True,
     install_requires=[
         'flask',
+        'flup'
     ],
 )
